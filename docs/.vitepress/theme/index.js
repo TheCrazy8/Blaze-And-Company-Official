bc903a3e8daf579924fde15ea49db5e644d9c3c4
Scarlett/Eight <bennettbashford@gmail.com>
2026-01-13 14:53:04 -0500

    Fix import paths for various components and styles

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,8 +1,8 @@
 import { h } from 'vue';
 import VPLTheme from '@lando/vitepress-theme-default-plus';
-import 'viewerjs/dist/viewer. min.css';
+import 'viewerjs/dist/viewer.min.css';
 import imageViewer from 'vitepress-plugin-image-viewer';
-import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer. vue';
+import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
 import { useRoute } from 'vitepress';
 import vitepressBackToTop from 'vitepress-plugin-back-to-top';
 import 'vitepress-plugin-back-to-top/dist/style.css';
@@ -16,14 +16,14 @@ import { NolebaseEnhancedReadabilitiesPlugin } from '@nolebase/vitepress-plugin-
 // Import all custom components
 import PWAPrompt from './components/PWAPrompt.vue';
 import FireParticles from './components/FireParticles.vue';
-import CursorTrail from './components/CursorTrail. vue';
+import CursorTrail from './components/CursorTrail.vue';
 import FloatingActionButton from './components/FloatingActionButton.vue';
 import ThemeSwitcher from './components/ThemeSwitcher.vue';
-import AnnouncementBanner from './components/AnnouncementBanner. vue';
+import AnnouncementBanner from './components/AnnouncementBanner.vue';
 import ProgressBar from './components/ProgressBar.vue';
 import Breadcrumbs from './components/Breadcrumbs.vue';
 import FeedbackWidget from './components/FeedbackWidget.vue';
-import ShareButtons from './components/ShareButtons. vue';
+import ShareButtons from './components/ShareButtons.vue';
 
 import './styles/main.css';
 
