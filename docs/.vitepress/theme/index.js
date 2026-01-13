5a0e43548e69462af26182b4a4bdcbbef5fa1ce3
Scarlett/Eight <bennettbashford@gmail.com>
2026-01-13 14:25:46 -0500

    Refactor theme index.js for enhanced readability

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,46 +1,32 @@
 import { h } from 'vue';
 import VPLTheme from '@lando/vitepress-theme-default-plus';
-import 'viewerjs/dist/viewer. min.css';
+import 'viewerjs/dist/viewer.min.css';
 import imageViewer from 'vitepress-plugin-image-viewer';
-import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer. vue';
+import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
 import { useRoute } from 'vitepress';
 import vitepressBackToTop from 'vitepress-plugin-back-to-top';
 import 'vitepress-plugin-back-to-top/dist/style.css';
 import '@nolebase/vitepress-plugin-enhanced-readabilities/client/style.css';
-import { 
-  LayoutMode,
-  NolebaseEnhancedReadabilitiesPlugin,
-  InjectionKey as NolebaseEnhancedReadabilitiesInjectionKey 
-} from '@nolebase/vitepress-plugin-enhanced-readabilities/client';
-import './styles/main. css';
+import { NolebaseEnhancedReadabilitiesPlugin } from '@nolebase/vitepress-plugin-enhanced-readabilities/client';
+import PWAPrompt from './components/PWAPrompt.vue';
+import './styles/main.css';
 
 export default {
   extends: VPLTheme,
 
-  enhanceApp(ctx) {
-    // Configure enhanced readabilities with all options
-    ctx.app.provide(NolebaseEnhancedReadabilitiesInjectionKey, {
-      layoutSwitch: {
-        defaultMode: LayoutMode.SidebarWidthAdjustableOnly,
-        contentLayoutMaxWidth:  {
-          defaultMaxWidth: 100,
-        },
-      },
-      spotlight: {
-        defaultToggle: false,
-        defaultToggledOn: false,
-      },
+  Layout: () => {
+    return h(VPLTheme.Layout, null, {
+      'layout-bottom': () => h(PWAPrompt)
     });
-    
-    // Register the plugin FIRST
+  },
+
+  enhanceApp(ctx) {
     ctx.app.use(NolebaseEnhancedReadabilitiesPlugin);
     
-    // Then call parent theme's enhanceApp
     if (VPLTheme.enhanceApp) {
       VPLTheme.enhanceApp(ctx);
     }
     
-    // Register other components
     ctx.app.component('vImageViewer', vImageViewer);
     vitepressBackToTop({ threshold: 300 });
   },
